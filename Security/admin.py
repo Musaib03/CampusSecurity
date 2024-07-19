0af7517cd1f5e66ecf4f886a564032a4669f350f
Musaib03 <wanimusaib520@gmail.com>
2024-07-19 06:11:42 +0530

    models registered in admin.py

--- Security/admin.py
+++ Security/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Block, Shift, Employee, Assignment, Attendance, Leave
+
+# Register each model with the admin site
+admin.site.register(Block)
+admin.site.register(Shift)
+admin.site.register(Employee)
+admin.site.register(Assignment)
+admin.site.register(Attendance)
+admin.site.register(Leave)
